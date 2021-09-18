9dbfbe078fef2ff0335b8fd2e2dee44697653ecd
thutanyan <thutathuta319@gmail.com>
2021-09-18 19:34:32 +0630

    9/18/2021

---
+++ floatactionbutton/js/app.js
@@ -0,0 +1,50 @@
+
+// UI
+
+const btns = document.querySelector('.btns');
+
+const btnprimary = document.querySelector('#btnprimary');
+
+
+
+btnprimary.addEventListener('click', () => {
+    // console.log('hay');
+
+    btns.classList.toggle('show');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
